87ebb0023a8373cb7b4128e082e64fc9e80aafec
Jesse-Alan-Hickman <jhickman@deakin.edu.au>
2022-09-25 19:20:22 +1000

    Condensing Website
    Webpages condensed into single html file. Login in page functionality complete. My Account functionality complete. Navigation functionality complete.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Login from "../components/login.vue";
-import Register from "../components/register.vue";
+//import Vue from "vue";
+//import VueRouter from "vue-router";
+//import Login from "../components/login.vue";
+//import Register from "../components/register.vue";
 
 
-new Vue ({
-    router,
-    store,
-    render: h => h(App)
-}).$mount("app");
+//new Vue ({
+ //   router,
+  //  store,
+   // render: h => h(App)
+//}).$mount("app");
