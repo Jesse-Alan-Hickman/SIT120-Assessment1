5c901d9fdad8de281adb5c16dac0dd9b3f11e051
Jesse-Alan-Hickman <jhickman@deakin.edu.au>
2022-09-30 00:37:38 +1000

    Tidy Up
    Consolidation on CSS files, general CSS file tidy up, comments added for better understanding, and unused component deleted.

--- components/login_button.js
+++
@@ -1,9 +0,0 @@
-Vue.component('login-button', {
-    template: '<button class="accessButton">{{ buttonText }}</button>',
-    props: {
-        buttonText: {
-            type: String,
-            default: () => "Error",
-        }
-    },
-})
